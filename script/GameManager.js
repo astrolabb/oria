6a31c21f5065a6e3c5d554e0b2bd7abf7b56979f
astrolabb4twitch <astrolabb4twitch@laposte.net>
2018-10-15 14:34:45 +0200

    gestion des clicks fonctionnelle

--- script/GameManager.js
+++ script/GameManager.js
@@ -5,6 +5,8 @@ function GameManager(monCanvas, data_interface, data_equilibrage)
     this.monCanvas = monCanvas;
     this.arrayOfGameObjects = [];
     this.map = {};
+    this.pos_x = 0;
+    this.pos_y = 0;
 
 }
 
@@ -16,8 +18,13 @@ function GameManager(monCanvas, data_interface, data_equilibrage)
 GameManager.prototype.setup = function ()
 {
   var self = this;
+  this.monCanvas.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  this.monCanvas.beginPath();
   this.map = new Map(this.monCanvas, self, data_interface),
   this.affichage_map();
+  this.click_canvas();
+  this.affichage_curseur();
+  this.monCanvas.closePath();
 }
 
 /**
@@ -26,13 +33,53 @@ GameManager.prototype.setup = function ()
  * @return {[type]} [description]
  */
  GameManager.prototype.affichage_map = function ()
- {
+  {
+    console.log("affichage_map : affichage des icones");
       for (var i in this.arrayOfGameObjects) {
-        console.log("this "+this.arrayOfGameObjects);
-        console.log("this.map "+this.map.largeur);
-        console.log("this.arrayOfGameObjects "+this.arrayOfGameObjects[i]);
-        console.log("this.map[] "+this.map[this.arrayOfGameObjects[i]]);
-
-        this.map[this.arrayOfGameObjects[i]].draw(this.monCanvas);
+          this.map[this.arrayOfGameObjects[i]].draw(this.monCanvas);
       }
   }
+  /**
+   * position curseur
+   *
+   * @return {[type]} [description]
+   */
+   GameManager.prototype.position_curseur = function(e)
+   {
+     console.log("mesure pixel cliqué");
+          this.pos_x = e.clientX;
+          this.pos_y = e.clientY;
+   }
+   /**
+    * Affichage curseur
+    *
+    * @return {[type]} [description]
+    */
+    GameManager.prototype.affichage_curseur = function()
+    {
+      console.log("affichage position curseur");
+      this.monCanvas.fillStyle = "orange";
+      this.monCanvas.rect(this.pos_x-1, 0, 2,  window.innerHeight);
+      this.monCanvas.stroke();
+      this.monCanvas.rect(0, this.pos_y-1, window.innerWidth,  2);
+      this.monCanvas.stroke();
+      this.monCanvas.arc(this.pos_x,this.pos_y,10,0,2*Math.PI, false);
+      this.monCanvas.stroke();
+
+    }
+    /**
+     * click sur le canvas
+     *
+     * @return {[type]} [description]
+     */
+     GameManager.prototype.click_canvas = function()
+     {
+       console.log("click");
+        var self = this;
+        $("#monCanvas").click(function(e){
+          self.position_curseur(e);
+          $("#monCanvas").off('click');
+          self.setup();
+        });
+
+     }
--- script/main.js
+++ script/main.js
@@ -8,10 +8,10 @@ function Main(data_interface, data_equilibrage){
   var monCanvas = $("#monCanvas")[0].getContext("2d");
   monCanvas.canvas.width  = window.innerWidth;
   monCanvas.canvas.height = window.innerHeight;
-  monCanvas.beginPath();
+
 
   var mon_GameManager = new GameManager(monCanvas, data_interface, data_equilibrage);
   mon_GameManager.setup();
 
-  monCanvas.closePath();
+
 }
